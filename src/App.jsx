747b6af3f78163ba796361e197810bef9cd0f664
Ben-starwar <chengdevith5@gmail.com>
2025-02-26 20:23:50 +0700

    first init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import { Card } from "flowbite-react";
+const App = () => {
   return (
-    <>
-      
-    </>
+    <Card href="#" className="max-w-sm">
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      Noteworthy technology acquisitions 2021
+    </h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </Card>
   )
 }
 
