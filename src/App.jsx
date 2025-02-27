d9f22e1bf9f4a14e476328c5ea7508c6e25989ab
ranntharath <tharath8061@gmail.com>
2025-02-27 16:49:38 +0700

    add : user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import NavComponent from './components/NavComponent';
 import FooterComponent from './components/FooterComponent';
+import Home from './pages/Home';
 const App = () => {
   return (
     <section >
-      <NavComponent/>
+      <Home/>
       <FooterComponent/>
-      <h1>helow</h1>
-      <h2>helow</h2>
-      <h3>helow</h3>
-      <h4>helow</h4>
-      <h5>helow</h5>
-      <p className='description'>From intuitive design features to built-in marketing tools, discover the complete solution for building an online portfolio that stands out.</p>
     </section>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavComponent from '../components/NavComponent'
+
+function Home() {
+  return (
+    <section>
+        <NavComponent/>
+    </section>
+  )
+}
+
+export default Home
