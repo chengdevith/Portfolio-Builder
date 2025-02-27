1e5d16d33af09a29a2b40c32de5692e6e7fea0e4
Sokneang080824 <laysokneang80@gmail.com>
2025-02-27 15:50:08 +0700

    add: still process

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Card from './components/CardComponent'
+import Data from './components/CardComponent'
 const App = () => {
 
-  const Data = {
+const Data = {
+    id: 1,
     image: 'https://colorlib.com/wp/wp-content/uploads/sites/2/knuford-free-template-408x322.jpg.avif',
     title: 'Knuford',
     categories: 'Business, Agency',
     template: 'Portfolio',
   };
+
   return (
     <>
     <div className="flex justify-center items-center h-screen bg-gray-100">
--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Card = ({ image, title, categoies, template}) => {
-// safsfs
   return (
+
     <div className="border border-gray-300 rounded-lg p-4 w-64 text-center shadow-md">
       <img src={`${image}`} className="text-xl font-semibold mb-2"></img>
       <h3 className="text-gray-700 mb-2">{title}</h3>
