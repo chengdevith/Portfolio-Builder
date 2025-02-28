2543f642ad6599908a1d3febd4de475c6e293142
Thai Lineth <thailinet08@gmail.com>
2025-02-28 12:57:42 +0700

    login Sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import NavComponent from './components/NavComponent';
-import FooterComponent from './components/FooterComponent';
+import React from "react";
+import NavComponent from "./components/NavComponent";
+import FooterComponent from "./components/FooterComponent";
 import { Card } from "flowbite-react";
-import Blogs from './pages/Blog';
+import Login from "./pages/auth/Login";
 const App = () => {
   return (
-    <section >
-      <NavComponent/>
-      <FooterComponent/>
-      <h1>helow</h1>
-      <h2>helow</h2>
-      <h3>helow</h3>
-      <h4>helow</h4>
-      <h5>helow</h5>
-      <p className='description'>From intuitive design features to built-in marketing tools, discover the complete solution for building an online portfolio that stands out.</p>
-      <Blogs/>
-    </section>
-  )
-}
+  <div className="flex justify-center items-center h-screen">
+    <Login/>
+  </div>
+ 
+  );
+};
 
-export default App
+export default App;
