73f5ca5f31181e935db55aa431fcddef8b6234d7
Ben-starwar <chengdevith5@gmail.com>
2025-02-28 16:47:41 +0700

    fix: login sign up

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,33 +1,25 @@
 import React, { useState } from "react";
-
+import { FcGoogle } from "react-icons/fc";
 export default function Login() {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <div className={` container ${isActive ? "active" : ""}`} id="container">
       {/* Sign Up Form */}
-      <div className="form-container sign-up">
+      <div className="form-container sign-up ">
         <form>
           <h2>Create Account</h2>
-          <div className="social-icons">
-            <a href="#" className="icon">
-              <i className="fa fa-google"></i>
-            </a>
-            <a href="#" className="icon">
-              <i className="fa fa-facebook"></i>
-            </a>
-            <a href="#" className="icon">
-              <i className="fa fa-github"></i>
-            </a>
-            <a href="#" className="icon">
-              <i className="fa fa-linkedin"></i>
-            </a>
-          </div>
-          <span>or use your email for registration</span>
+
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <button type="submit">Sign Up</button>
+          <button className="btn mb-5" type="submit">
+            Sign Up
+          </button>
+          <span className="mb-2 text-color-description">Or</span>
+          <button className="!bg-transparent !mt-0">
+            <FcGoogle className="text-4xl" />
+          </button>
         </form>
       </div>
 
@@ -35,25 +27,20 @@ export default function Login() {
       <div className="form-container sign-in">
         <form>
           <h2>Sign In</h2>
-          <div className="social-icons">
-            <a href="#" className="icon">
-              <i className="fa fa-google"></i>
-            </a>
-            <a href="#" className="icon">
-              <i className="fa fa-facebook"></i>
-            </a>
-            <a href="#" className="icon">
-              <i className="fa fa-github"></i>
-            </a>
-            <a href="#" className="icon">
-              <i className="fa fa-linkedin"></i>
-            </a>
-          </div>
-          <span>or use your email password</span>
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <a href="#">Forget Your Password?</a>
-          <button type="submit">Sign In</button>
+          <div className=" flex justify-end w-full">
+            <a className="sub-description mt-2 hover:!text-color-secondary" href="#">
+              Forget Your Password?
+            </a>
+          </div>
+          <button className="btn mb-5" type="submit">
+            Sign In
+          </button>
+          <span className="mb-2 text-color-description">Or login with</span>
+          <button className="!bg-white flex items-center gap-3 ring-slate-400 !ring-2 !py-2 !px-4 !rounded-3xl">
+            <FcGoogle className="text-4xl" /> <span className="text-black !normal-case">Google</span>
+          </button>
         </form>
       </div>
 
@@ -63,12 +50,16 @@ export default function Login() {
           <div className="toggle-panel toggle-left">
             <h1>Welcome Back!</h1>
             <p>Enter your personal details to use all site features</p>
-            <button onClick={() => setIsActive(false)}>Sign In</button>
+            <button className="btn" onClick={() => setIsActive(false)}>
+              Sign In
+            </button>
           </div>
           <div className="toggle-panel toggle-right">
             <h1>Hello, Friend!</h1>
             <p>Register with your personal details to use all site features</p>
-            <button onClick={() => setIsActive(true)}>Sign Up</button>
+            <button className="btn" onClick={() => setIsActive(true)}>
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
