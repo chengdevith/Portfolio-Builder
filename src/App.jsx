41ba20fd614fb82515de7c37c408769dcca2f1ca
Sokneang080824 <laysokneang80@gmail.com>
2025-02-28 16:42:44 +0700

    Template: Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
-import React from 'react'
-import NavComponent from './components/NavComponent';
-import FooterComponent from './components/FooterComponent';
-import Card from './components/CardComponent'
-import Data from './components/CardComponent'
-const App = () => {
+//-----------------------------------------------
+import React from 'react';
+import CardComponent from './components/Card/CardComponent';
+import Template from './pages/Template';
+//-----------------------------------------------
 
-const Data = {
-    id: 1,
-    image: 'https://colorlib.com/wp/wp-content/uploads/sites/2/knuford-free-template-408x322.jpg.avif',
-    title: 'Knuford',
-    categories: 'Business, Agency',
-    template: 'Portfolio',
-  };
+function App() {
+
 
   return (
-    <>
-    <section >
-      <NavComponent/>
-      <FooterComponent/>
-      <h1>helow</h1>
-      <h2>helow</h2>
-      <h3>helow</h3>
-      <h4>helow</h4>
-      <h5>helow</h5>
-      <p className='description'>From intuitive design features to built-in marketing tools, discover the complete solution for building an online portfolio that stands out.</p>
-    </section>
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <Card {...Data} />
+    <div>
+      <Template />
+      <CardComponent />
     </div>
-    </>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Template.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { ResumeComponent } from "../components/Card/helpers/ButtonComponent";
+import { PortfolioComponent } from "../components/Card/helpers/ButtonComponent";
+import { ActiveButton } from "../components/Card/helpers/ButtonComponent";
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.0/flowbite.min.js"></script>;
+
+export default function Template() {
+  return (
+    <section>
+      <div className="p-6 bg-gray-100 min-h-auto mb-20">
+        <div className="max-w-4xl mx-auto text-center mb-12">
+          <h1 className="text-4xl font-bold mb-4 text-purple-600">
+            Need inspiration?
+          </h1>
+          <p className="text-gray-600 mb-6">
+            See how the creatives below customized their Portfolio themes.
+          </p>
+        </div>
+
+        <div className="container m-auto max-w-screen-lg flex justify-between items-center px-14">
+          <div className="">
+            <div className="flex space-x-4 items-center">
+              {/* <button
+                type="button"
+                class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
+              >
+                All Fields
+              </button> */}
+
+              <ActiveButton />
+
+              {/* dropdown  */}
+              <ResumeComponent/>
+              <PortfolioComponent/>
+
+
+              {/* <button class="relative inline-flex items-center justify-center p-0.5  me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                  Portfolio <FontAwesomeIcon icon={faChevronDown} />
+                  </span>
+              </button> */}
+            </div>
+          </div>
+
+          <form class=" w-1/2">
+            <label
+              for="default-search"
+              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div class="relative p-0.5">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                class="block w-full py-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos..."
+                required
+              />
+              <button
+                type="submit"
+                class="pt-1 text-white absolute end-2.5 bottom-2.5 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
