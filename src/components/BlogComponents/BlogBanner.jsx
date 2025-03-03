23c4ead3444d2b86ec3ae314c74449564ef6ba96
Thai Lineth <thailinet08@gmail.com>
2025-03-03 19:52:22 +0700

    saved

--- src/components/BlogComponents/BlogBanner.jsx
+++ src/components/BlogComponents/BlogBanner.jsx
@@ -6,15 +6,15 @@ const BlogBanner = () => {
     <div className="w-full">
       {/* Hero Banner Section */}
       <section
-        className="relative bg-cover bg-center text-white h-screen flex items-center justify-start"
+        className="relative bg-cover bg-center text-white h-screen text-left flex items-center justify-start"
         style={{ backgroundImage: `url(${bannerImage})` }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-60"></div>
         <div className="relative z-10 px-4 md:px-10 lg:px-36 pb-10 lg:pb-20">
-          <h2 className="text-3xl md:text-5xl font-bold leading-tight animate-slideInLeft">
+          <h2 className="text-[36px] md:text-5xl font-bold leading-tight text-center">
             Introducing PortfolioBuilder: <br /> Crafting Digital Experiences <br /> with Passion
           </h2>
-          <p className="mt-4 text-[20px] text-color-body max-w-lg mx-auto animate-fadeIn">
+          <p className="mt-4 text-[20px] text-color-body text-center max-w-lg mx-auto">
             Welcome to the launch of PortfolioBuilder, your new go-to platform for creating stunning, personal portfolios with ease.
           </p>
         </div>
