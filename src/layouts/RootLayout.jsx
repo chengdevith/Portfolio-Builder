129524197ffa7f4361f3d13e4ab04ec5279ce078
Sokneang080824 <laysokneang80@gmail.com>
2025-03-03 14:11:49 +0700

    add: about page

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import NavComponent from '../components/NavComponent'
+import { Outlet } from 'react-router-dom'
+import FooterComponent from '../components/FooterComponent'
+
+const RootLayout = () => {
+  return (
+    <>
+    <NavComponent/>
+    <main>
+        <Outlet/>
+    </main>
+    <footer>
+        <FooterComponent/>
+    </footer>
+    </>
+  )
+}
+
+export default RootLayout
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,33 +1,25 @@
-
-import React from 'react'
-import NavComponent from '../components/NavComponent'
-import HeroBanner from '../components/AboutComponents/HeroBanner'
-import About_Project from '../components/AboutComponents/About_Project'
-import Our_Vision from '../components/AboutComponents/Our_Vision'
-import Our_Mission from '../components/AboutComponents/Our_Mission'
-import Mentor from '../components/AboutComponents/Mentor'
-import Our_Team from '../components/AboutComponents/Our_Team'
-import Our_Values from '../components/AboutComponents/Our_Values'
-import Contact_Us from '../components/AboutComponents/Contact_Us'
+import React from "react";
+import NavComponent from "../components/NavComponent";
+import HeroBanner from "../components/AboutComponents/HeroBanner";
+import About_Project from "../components/AboutComponents/About_Project";
+import Our_Vision from "../components/AboutComponents/Our_Vision";
+import Our_Mission from "../components/AboutComponents/Our_Mission";
+import Mentor from "../components/AboutComponents/Mentor";
+import Our_Team from "../components/AboutComponents/Our_Team";
+import Our_Values from "../components/AboutComponents/Our_Values";
+import Contact_Us from "../components/AboutComponents/Contact_Us";
 
 export default function About() {
   return (
     <>
-      <header>
-        <NavComponent/>
-      </header>
-      
-      <main>
-        <HeroBanner/>
-        <About_Project/>
-        <Our_Vision/>
-        <Our_Mission/>
-        <Mentor/>
-        <Our_Team/>
-        <Our_Values/>
-        <Contact_Us/>
-      </main>
-
+      <HeroBanner />
+      <About_Project />
+      <Our_Vision />
+      <Our_Mission />
+      <Mentor />
+      <Our_Team />
+      <Our_Values />
+      <Contact_Us />
     </>
   );
 }
