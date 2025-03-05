6e43338c97f513b203c3ed547502bdcad8c61bed
Ben-starwar <chengdevith5@gmail.com>
2025-03-05 14:31:52 +0700

    05-03-25 v2

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import Toggle from "./Toggle";
 import "./auth.css"
 export default function Login() {
   const [isActive, setIsActive] = useState(false);
