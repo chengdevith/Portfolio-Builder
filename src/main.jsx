7b222f4e752f23b2a4c08a5ddc62958f9ad761d5
Ben-starwar <chengdevith5@gmail.com>
2025-03-05 14:19:58 +0700

    add: aboutLayout v3

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App.jsx";
-import RootLayout from "./Layout/RootLayout.js";
+import RootLayout from "./Layout/RootLayout.jsx"
 import About from "./pages/About.jsx";
 import Blog from "./pages/Blog.jsx";
 import Template from "./pages/Template.jsx";
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <Routes>
-        <Route element={<RootLayout />}>
+        <Route element={<RootLayout/>}>
           <Route path="/" element={<App />} />
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
