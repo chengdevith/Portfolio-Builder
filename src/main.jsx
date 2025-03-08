4be394ea9fb7cc4ca70ebaaaf6b114278a6deacc
Ben-starwar <chengdevith5@gmail.com>
2025-03-08 13:12:28 +0700

    signUp with google

--- src/main.jsx
+++ src/main.jsx
@@ -15,25 +15,31 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import RequestResetPassword from "./pages/auth/RequestResetPassword.jsx";
 import ResetPassword from "./pages/auth/ResetPassword.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <StrictMode>
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route path="/" element={<App />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/template" element={<Template />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/otp" element={<OTPVerifyComponent />} />
-          <Route path="/request-reset-password" element={<RequestResetPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-        </Routes>
-      </StrictMode>
-    </BrowserRouter>
-  </Provider>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <StrictMode>
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route path="/" element={<App />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/template" element={<Template />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/otp" element={<OTPVerifyComponent />} />
+            <Route
+              path="/request-reset-password"
+              element={<RequestResetPassword />}
+            />
+            <Route path="/reset-password" element={<ResetPassword />} />
+          </Routes>
+        </StrictMode>
+      </BrowserRouter>
+    </Provider>
+  </GoogleOAuthProvider>
 );
