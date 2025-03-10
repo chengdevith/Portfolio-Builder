44edd28eef840293ad0ab7171d74d536a622100e
Thai Lineth <thailinet08@gmail.com>
2025-03-10 16:20:47 +0700

    save

--- src/mock-data/feature.js
+++ src/mock-data/feature.js
@@ -6,19 +6,19 @@ import { style } from "framer-motion/client";
 const FeatureData = [
   {
     id: 1,
-    image: "https://cdn-icons-png.flaticon.com/512/9709/9709605.png",
+    image: "https://cdn-icons-png.flaticon.com/512/3161/3161537.png",
     title: "Effortless & Free Creation",
     description: "Craft a pro portfolio fast—no coding, totally free!",
   },
   {
     id: 2,
-    image: "https://img.freepik.com/premium-vector/star-icon_874723-5.jpg",
+    image: "https://img.freepik.com/premium-vector/yellow-color-star-icon_1076610-30862.jpg",
     title: "Stunning Templates",
     description:"Pick gorgeous designs to highlight your work.",
   },
   {
     id: 3,
-    image: "https://cdn-icons-png.flaticon.com/512/9709/9709605.png",
+    image: "https://cdn-icons-png.freepik.com/512/302/302925.png",
     title: "Browser-Based Building",
     description: "Build & tweak anywhere, free—no downloads needed.",
   },
